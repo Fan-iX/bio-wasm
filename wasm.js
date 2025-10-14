d761af69d42cdfe9f7f71944dfc8271c4948b658
Fan-iX <Fan-iX@users.noreply.github.com>
2025-10-14 11:58:19 +0000

    deploy: f76f66c758f0636abce45f319ad9342e37084fe4

--- wasm.js
+++ wasm.js
@@ -1,2 +1,3 @@
 export { default as 'clustalw/clustalw2' } from './wasm/clustalw/clustalw2.js';
 export { default as 'kalign/kalign' } from './wasm/kalign/kalign.js';
+export { default as 'raxml/raxmlHPC' } from './wasm/raxml/raxmlHPC.js';
